897862286203fdc1aabae6a36d335f2928bd0cf7
Désirée Lussier <9257980+dllussier@users.noreply.github.com>
2019-08-13 11:02:26 -0400

    Update graph_vae_dgl_pytorch.py

--- graph_vae_dgl_pytorch.py
+++ graph_vae_dgl_pytorch.py
@@ -123,10 +123,10 @@ if CUDA:
 trainset = MiniGCDataset(320, 10, 20)
 testset = MiniGCDataset(80, 10, 20)
 
-train_loader = DataLoader(datasets.MNIST('data', train=True, download=True, transform=transforms.ToTensor()), 
+train_loader = DataLoader(trainset, train=True, download=True, transform=transforms.ToTensor()),
     batch_size=BATCH_SIZE, collate_fn=collate, shuffle=True, **kwargs)
 
-test_loader = DataLoader(datasets.MNIST('data', train=False, transform=transforms.ToTensor()),
+test_loader = DataLoader(testset, train=False, transform=transforms.ToTensor()),
     batch_size=BATCH_SIZE, collate_fn=collate, shuffle=True, **kwargs)
 
 def loss_function(recon_g, g, mu, log_var):
