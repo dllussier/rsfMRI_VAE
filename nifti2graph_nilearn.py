82b93bb96bf5d313df613ebfe388a9cd5f2111fb
Désirée Lussier <9257980+dllussier@users.noreply.github.com>
2019-10-25 11:58:49 -0400

    Update nifti2graph_nilearn.py

--- nifti2graph_nilearn.py
+++ nifti2graph_nilearn.py
@@ -6,6 +6,7 @@ import shutil
 import dgl
 import networkx as nx
 import numpy as np
+import matplotlib.pyplot as plt
 from nilearn import datasets
 from nilearn import plotting 
 from glob import glob
@@ -55,13 +56,27 @@ for t in tqdm(train):
 for t in tqdm(test):
     copyfile(t,os.path.join(test_dir,os.path.split(t)[1]))
 
-#designate mask
+#set up matrix plotting
+def plot_matrices(matrices, matrix_kind):
+    n_matrices = len(matrices)
+    fig = plt.figure(figsize=(n_matrices * 4, 4))
+    for n_subject, matrix in enumerate(matrices):
+        plt.subplot(1, n_matrices, n_subject + 1)
+        matrix = matrix.copy()  # avoid side effects
+        np.fill_diagonal(matrix, 0)
+        vmax = np.max(np.abs(matrix))
+        title = '{0}, subject {1}'.format(matrix_kind, n_subject)
+        plotting.plot_matrix(matrix, vmin=-vmax, vmax=vmax, cmap='RdBu_r',
+                             title=title, figure=fig, colorbar=False)
+
+#common helper objects
 masker = NiftiMapsMasker(maps_img=atlas_filename, standardize=True,
                          memory='nilearn_cache', verbose=5)
 correlation_measure = ConnectivityMeasure(kind='correlation')
-
+estimator = GraphLassoCV()
 coords = atlas.region_coords
 
+#generate graphs and save as numpy files for use in dataloader
 for s in [train_dir,test_dir]:
     func_files = glob(os.path.join(s,"*_func_preproc.nii.gz"))    
     for idx in tqdm(range(len(func_files))):
@@ -82,16 +97,23 @@ for s in [train_dir,test_dir]:
         np.save(corr_save, correlation_matrix, allow_pickle=True, fix_imports=True)
 
         #show connectivity matrix plot
-        #plot_matrices(correlation_matrix, 'correlation')
-        np.fill_diagonal(correlation_matrix, 0)
-        plotting.plot_matrix(correlation_matrix, labels=labels, 
-                             colorbar=True,vmax=0.8, vmin=-0.8)
+        plot_matrices(correlation_matrix, 'correlation')
         
-        #display corresponding graph keeping only 20% of edges with the highest value
-        plotting.plot_connectome(correlation_matrix, coords,
-                                 edge_threshold="80%", colorbar=True)
-        plotting.show()
-           
+        #compute covariance
+        estimator.fit(time_series)
+        
+        
+    
+        #plot_matrices(estimator.covariance_, 'covariance')
+        plotting.plot_matrix(estimator.covariance_, labels=labels,
+                             figure=(9, 7), vmax=1, vmin=-1,
+                             title='Covariance')
+
+        #display sparse inverse covariance
+        plotting.plot_matrix(-estimator.precision_, labels=labels,
+                             figure=(9, 7), vmax=1, vmin=-1,
+                             title='Sparse inverse covariance')
+        
     #remove original 4D files
     for f in func_files:      
         os.remove(f)
