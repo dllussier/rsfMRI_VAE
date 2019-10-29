6bfba7870206709dbf8b309534fbe19c3be7e6af
Désirée Lussier <9257980+dllussier@users.noreply.github.com>
2019-10-29 13:08:42 -0400

    Update np2graph.py

--- np2graph.py
+++ np2graph.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 """
 @author: lussier
 
@@ -18,42 +18,72 @@ from nilearn import plotting
 from glob import glob
 from tqdm import tqdm
 from shutil import copyfile
-from sklearn.model_selection import train_test_split
-from nilearn.input_data import NiftiMapsMasker
-from nilearn.connectome import ConnectivityMeasure
 
+#import atlas
+atlas = datasets.fetch_atlas_msdl()
+# Loading atlas image stored in 'maps'
+atlas_filename = atlas['maps']
+# Loading atlas data stored in 'labels'
+labels = atlas['labels']
 
-#draw connectome to networkx graph
-G = nx.MultiGraph()
+#set up matrix plotting
+def plot_matrices(matrices, matrix_kind):
+    n_matrices = len(matrices)
+    fig = plt.figure(figsize=(n_matrices * 4, 4))
+    for n_subject, matrix in enumerate(matrices):
+        plt.subplot(1, n_matrices, n_subject + 1)
+        matrix = matrix.copy()  # avoid side effects
+        np.fill_diagonal(matrix, 0)
+        vmax = np.max(np.abs(matrix))
+        title = '{0}, subject {1}'.format(matrix_kind, n_subject)
+        plotting.plot_matrix(matrix, labels=labels, vmin=-vmax, vmax=vmax, cmap='RdBu_r',
+                             title=title, figure=fig, colorbar=False)
 
-#load numpy array from saved .npy file
-a = np.load('./data/train/_0050964_correlations.npy', allow_pickle=False)
-a
-a.shape
+train_dir = './data/train/'
+test_dir = './data/test/'
 
-#reshape stacked numpy array to 2d 
-b = np.reshape(a, (39,39), order='C')
-b
-b.shape
+for s in [train_dir,test_dir]:
+    array_files = glob(os.path.join(s,"*_correlations.npy"))    
+    for idx in tqdm(range(len(array_files))):
+        array_data = array_files[idx]
+        array_name = re.findall(r'_\d+',array_data)[0]
 
-#verify that the matrices have not changed
-plot_matrices(a, 'original')
-plot_matrices(a, 'reshaped')
+        #draw connectome to networkx graph
+        G = nx.MultiGraph()
 
-#convert reshaped numpy array to networkx graph 
-D = nx.nx.convert.to_networkx_graph(b, create_using=nx.MultiGraph)
-keys = G.add_edges_from(D.edges)
+        #load numpy array from saved .npy file
+        a = np.load('./data/train/_0050964_correlations.npy', allow_pickle=True)
+        print('Saved correlations are in an array of shape {0}'.format(a.shape))
+        print(a)
 
-#view networkx graph
-nx.draw(G, with_labels=True)
+        #reshape stacked numpy array to 2d 
+        b = np.reshape(a, (39,39), order='C')
+        print('Reshaped correlation matrix is in an array of shape {0}'.format(b.shape))
+        print(b)
 
-#verify number of nodes is consistent with numpy shape
-nx.number_of_nodes(G)
-
-
-
-#convert nx graph to dgl
-g_dgl = dgl.DGLGraph(G)
-
-#view dgl graph
-nx.draw(g_dgl.to_networkx(), with_labels=True)
+        #verify that the matrices have not changed
+        plot_matrices(a, 'original')
+        plot_matrices(a, 'reshaped')
+        
+        #convert reshaped numpy array to networkx graph 
+        D = nx.nx.convert.to_networkx_graph(b, create_using=nx.MultiGraph)
+        keys = G.add_edges_from(D.edges)
+        
+        #verify number of nodes is consistent with numpy shape
+        print('For %d the node count is {0}'.format(nx.number_of_nodes(G))) #% subject)
+        
+        #view networkx graph
+        plt.figure()
+        nx.draw(G, with_labels=True)
+        #plt.show()
+        
+        #convert nx graph to dgl
+        g_dgl = dgl.DGLGraph(G)
+        
+        #view dgl graph
+        plt.figure()
+        nx.draw(g_dgl.to_networkx(), with_labels=True)
+        #plt.show()
+        
+        #save graph as file for use by dataloader
+        ####torch.save
