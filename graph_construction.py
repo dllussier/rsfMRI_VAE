b4e5fb337d80763f4f957845884693d710411539
Désirée Lussier <9257980+dllussier@users.noreply.github.com>
2019-12-11 13:18:28 -0500

    Update graph_construction.py

--- graph_construction.py
+++ graph_construction.py
@@ -39,32 +39,33 @@ sbl_dir = os.path.join(connectome_dir, 'sbl')
 for s in [pitt_dir,olin_dir,ohsu_dir,sdsu_dir,trinity_dir,um_1_dir,um_2_dir,
           usm_dir,yale_dir,cmu_dir,leuven_1_dir,leuven_2_dir,kki_dir,nyu_dir,
           stanford_dir,ucla_1_dir,ucla_2_dir,maxmun_dir,caltech_dir,sbl_dir]:
-    array_files = glob(os.path.join(s,"*.mat"))    
-    for idx in tqdm(range(len(array_files))):
-        array_data = array_files[idx]
-        array_name = re.findall(r'subject_\d+',array_data)[0]
+    connectome_files = glob(os.path.join(s,"*.mat"))    
+    for idx in tqdm(range(len(connectome_files))):
+        connectomes = connectome_files[idx]
+        connectome_name = re.findall(r'subject_\d+',connectomes)[0]
 
         #draw connectome to networkx graph
         G = nx.MultiGraph()
 
         #load numpy array from saved .npy file
-        a = np.load(array_data, allow_pickle=True)
+        a = scipy.io.loadmat(connectomes)
+        #a = np.load(connectomes, allow_pickle=True)
         print('Saved correlations are in an array of shape {0}'.format(a.shape))
         print(a)
 
-        #reshape stacked numpy array to 2d 
-        b = np.reshape(a, (39,39), order='C')
-        print('Reshaped correlation matrix is in an array of shape {0}'.format(b.shape))
-        print(b)
+        ###reshape stacked numpy array to 2d 
+        #b = np.reshape(a, (39,39), order='C')
+        #print('Reshaped correlation matrix is in an array of shape {0}'.format(b.shape))
+        #print(b)
         
         #convert reshaped numpy array to networkx graph 
-        D = nx.nx.convert.to_networkx_graph(b, create_using=nx.MultiGraph)
+        D = nx.nx.convert.to_networkx_graph(a, create_using=nx.MultiGraph)
         keys = G.add_edges_from(D.edges)
         
         #verify number of nodes is consistent with numpy shape
-        print('For the graph converted from %s the node count is {0}'.format(nx.number_of_nodes(G)) % array_data)
+        print('For the graph converted from %s the node count is {0}'.format(nx.number_of_nodes(G)) % connectomes)
         
         #save graph as file for use by dataloader
-        array_save = os.path.join(s, f'{array_name}.gpickle')
-        nx.write_gpickle(G, array_save)   
-        print('Graph pickle saved as %s' % array_save)
+        graph_save = os.path.join(s, f'{connectome_name}.gpickle')
+        nx.write_gpickle(G, graph_save)   
+        print('Graph pickle saved as %s' % graph_save)
