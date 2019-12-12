b651fd34e4a7169d8bd6dcd9d1d721deffab479b
Désirée Lussier <9257980+dllussier@users.noreply.github.com>
2019-12-12 15:03:04 -0500

    Update graph_construction.py

--- graph_construction.py
+++ graph_construction.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """
 @author: lussier
-
-uses previously save .npy files to generate graphs that are saved for use by dataloader
+uses previously generated .mat files from NIAK connectome pipeline to generate graphs that are saved for use by dataloader
 """
 
 import os
@@ -14,35 +13,46 @@ from glob import glob
 from tqdm import tqdm
 
 #define data folders containing connectomes
-connectome_dir = './output_abide_connectomes/graph_prop/'
+connectome_dir = './output_abide_connectomes/'
 pitt_dir = os.path.join(connectome_dir, 'pitt')
 olin_dir = os.path.join(connectome_dir, 'olin')
-ohsu_dir = os.path.join(connectome_dir, 'ohsu')
+#ohsu_dir = os.path.join(connectome_dir, 'ohsu')
 sdsu_dir = os.path.join(connectome_dir, 'sdsu')
 trinity_dir = os.path.join(connectome_dir, 'trinity')
-um_1_dir = os.path.join(connectome_dir, 'um_1')
-um_2_dir = os.path.join(connectome_dir, 'um_2')
+#um_1_dir = os.path.join(connectome_dir, 'um_1')
+#um_2_dir = os.path.join(connectome_dir, 'um_2')
 usm_dir = os.path.join(connectome_dir, 'usm')
 yale_dir = os.path.join(connectome_dir, 'yale')
-cmu_dir = os.path.join(connectome_dir, 'cmu')
+#cmu_dir = os.path.join(connectome_dir, 'cmu')
 leuven_1_dir = os.path.join(connectome_dir, 'leuven_1')
 leuven_2_dir = os.path.join(connectome_dir, 'leuven_2')
 kki_dir = os.path.join(connectome_dir, 'kki')
 nyu_dir = os.path.join(connectome_dir, 'nyu')
-stanford_dir = os.path.join(connectome_dir, 'stanford')
+#stanford_dir = os.path.join(connectome_dir, 'stanford')
 ucla_1_dir = os.path.join(connectome_dir, 'ucla_1')
 ucla_2_dir = os.path.join(connectome_dir, 'ucla_2')
 maxmun_dir = os.path.join(connectome_dir, 'maxmun')
 caltech_dir = os.path.join(connectome_dir, 'caltech')
 sbl_dir = os.path.join(connectome_dir, 'sbl')
 
-for s in [pitt_dir,olin_dir,ohsu_dir,sdsu_dir,trinity_dir,um_1_dir,um_2_dir,
-          usm_dir,yale_dir,cmu_dir,leuven_1_dir,leuven_2_dir,kki_dir,nyu_dir,
-          stanford_dir,ucla_1_dir,ucla_2_dir,maxmun_dir,caltech_dir,sbl_dir]:
-    connectome_files = glob(os.path.join(s,"*.mat"))    
+for s in [pitt_dir,olin_dir,sdsu_dir,trinity_dir,#ohsu_dir,um_1_dir,um_2_dir,cmu_dir,
+          yale_dir,usm_dir,leuven_1_dir,leuven_2_dir,kki_dir,nyu_dir,#stanford_dir,
+          ucla_1_dir,ucla_2_dir,maxmun_dir,caltech_dir,sbl_dir]:
+#    connectome_files = glob(os.path.join(s,"graph_prop/*.mat"))
+
+    graph_prop_dir = os.path.join(s,"graph_prop/")
+    graph_mat_file = glob(os.path.join(graph_prop_dir,"*.mat"))
+    graph_mat = loadmat(graph_mat_file)
+    graph_mat
+    
+    conn_dir = os.path.join(s,"connectomes/")
+    connectome_mat_file = glob(os.path.join(conn_dir,"*.mat"))
+    connectome_mat = loadmat(connectome_mat_file)
+    connectome_mat    
+
     for idx in tqdm(range(len(connectome_files))):
         connectomes = connectome_files[idx]
-        connectome_name = re.findall(r'subject_\d+',connectomes)[0]
+        connectome_name = re.findall(r'graph_prop_rois_sub\d+',connectomes)[0]
 
         #draw connectome to networkx graph
         G = nx.MultiGraph()
